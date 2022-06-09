dc86d437714802288e8317176578fb1a96b49d0d
Josh Burd <Jburdt@gmail.com>
2022-06-09 18:58:20 -0400

    commit tree 47d4cd3c3183e456132112301f1b18592dacad1e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (driverArray) => {
+    return (driverArray.slice(0, 2))
+}
+// CORRECT WAY TO CODE THE LESSON
+// const returnFirstTwoDrivers = (drivers) => {
+//   return (drivers.slice(0, 2))
+//} 
+
+const returnLastTwoDrivers = (driverArray) => {
+    return (driverArray.slice(-2))
+}
+//  CORRECT WAY TO CODE THE LESSON
+//  const returnLastTwoDrivers = (driverArray) => {
+//      returnLastTwoDrivers (driverArray.slice(-2))
+//  }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+    
+const createFareMultiplier = (int) => {
+    return function (fareMultiplier) {
+        return fareMultiplier * int 
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+// CORRECT WAY TO CODE
+// driver = the array in the test on the back end
+// function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+//     return returnFirstTwoDrivers(drivers)
+// }
+
+const selectDifferentDrivers = (driverArray, drivers) => {
+    return (drivers(driverArray))   
+}
+
+    
+   
